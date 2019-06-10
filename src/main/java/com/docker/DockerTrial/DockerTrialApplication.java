e9a2edbc353c29a2c4a5013467ab8e5c699ba390
Mahima Choudha <mahimac@cdac.in>
2019-06-10 11:51:19 +0530

    Hello World Spring boot web service

---
+++ src/main/java/com/docker/DockerTrial/DockerTrialApplication.java
@@ -0,0 +1,27 @@
+package com.docker.DockerTrial;
+
+import java.util.Date;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DockerTrialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerTrialApplication.class, args);
+    }
+}
+
+@RestController
+class HelloDockerRestController {
+
+    @RequestMapping("/hello/{name}")
+    public String helloDocker(@PathVariable(value = "name") String name) {
+        String response = "Hello " + name + " Response received on : " + new Date();
+        System.out.println(response);
+        return response;
+    }
+}
---
+++ src/test/java/com/docker/DockerTrial/DockerTrialApplicationTests.java
@@ -0,0 +1,16 @@
+package com.docker.DockerTrial;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DockerTrialApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
